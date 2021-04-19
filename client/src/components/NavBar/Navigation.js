8d557bb03f5fa0c1d81fb26cf7f08dccb5869dd0
Jared Seefried <jared.seefried@yahoo.com>
2021-04-19 14:07:54 -0600

    created navbar component. Filler info in there for now

---
+++ client/src/components/NavBar/Navigation.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Navbar from 'react-bootstrap/Navbar'
+import './Navigation.css';
+
+
+function Navigation(props) {
+  return (
+    <Navbar bg='light' collapseOnSelect expand='md' sticky="top" varient='light'>
+      <Navbar.Brand href="#home">My CBD Store</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+        <Nav>
+          <Nav.Link href="#deets">Sign Up</Nav.Link>
+          <Nav.Link eventKey={2} href="#memes">
+            Log In
+      </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Navigation;
